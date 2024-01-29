6352dd3cf1cab531ec5522aed01a84983e224de8
PatrykPańczuk <p.panczuk1@gmail.com>
2024-01-29 14:21:21 +0100

    Project init

---
+++ src/main/java/pl/edu/wszib/book/app/spring/configuration/AppConfiguration.java
@@ -0,0 +1,10 @@
+package pl.edu.wszib.book.app.spring.configuration;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan("pl.edu.wszib.book.app.spring")
+
+public class AppConfiguration {
+}
